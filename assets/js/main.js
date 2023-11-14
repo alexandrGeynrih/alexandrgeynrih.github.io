42dc5d4e32decb0fe42e974ccec61d372154c98d
Alex <alex@genrih.com>
2023-11-14 20:50:36 +0500

    add notification

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,6 +167,25 @@ accordeons.forEach((item) => {
     });
 });
 
+//Notification
+const notificationBtn = document.querySelectorAll('[data-show-notification]');
+notificationBtn.forEach((item) => {
+    item.addEventListener("click", function (event) {
+        const id = this.dataset.showNotification.replace('#', '');        
+        if(document.getElementById(id)){
+            document.getElementById(id).classList.add('notification_active');
+        }        
+    });
+});
+
+const notificationCloseBtn = document.querySelectorAll('.notification_close');
+notificationCloseBtn.forEach((item) => {
+    item.addEventListener("click", function (event) {
+       this.closest(".notification_active").classList.remove('notification_active');
+        
+    });
+});
+
 
 if ($(".js-select").length) {
     $(".js-select").styler({
